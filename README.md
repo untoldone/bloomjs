b1b9e2df7a43d179117349a2d33755fa7c9e8a8b
Michael M. Wasser <untoldone@users.noreply.github.com>
2015-03-10 17:07:49 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,141 @@
+BloomJS
+=======
+
+A browser-based and node.js client to [BloomAPI](http://www.bloomapi.com).
+
+# Setup
+
+The following are steps to get BloomJS into your javascript-based project.
+
+## Browser
+Place `dist/bloom.min.js` from the GitHub project or install it with bower via `bower install bloomjs` into your project in a location accessible from the web. Include it in your site with a script tag
+
+    <script src="js/bloom.min.js"></script>
+
+## Node.js
+
+    npm install bloomjs
+
+Use it in your node project via
+
+    var bloomjs = require('bloomjs');
+    
+# Client API
+
+**Create New Client**
+
+`bloomjs.Client(<API Key OR options>);`
+
+* Returns: BloomJS Client Object
+* API Key expected to be string
+* Options object can have
+  * 'url': root url of BloomAPI (e.g. http://www.bloomapi.com/api)
+  * 'apiKey': API Key
+
+###BloomJS Client Object
+
+`bloomClient.sources(<callback>)`
+* Returns nothing
+* `callback` a function that will be called once results retrieved. Paramters to callback include
+  * `error` an object that is set if something went wrong
+  * `response` an array of sources available
+
+`bloomClient.search(<source>, <search params>, <options>, <callback>)`
+
+* Returns nothing
+* `source` is a string that matches a BloomAPI data source name (e.g. 'usgov.hhs.npi')
+* `search params` is a object that represnts a query. See *Example Usage* for expected object structure. *Optional*
+* `options` object with options to pass through that are not search params (e.g. 'offset' and 'limit') *Optional*
+* `callback` a function that will be called once results retrieved. Parameters to callback include
+  *  `error` an object that is set if something went wrong
+  *  `response` an array of results found given query
+  *  `info` additional information on the query/ response. Currently includes `meta` which is the metadata from the response (e.g. `rowCount` and `messages`)
+
+`bloomClient.find(<source>, <id>, <callback>)`
+* Returns nothing
+* `source` is a string that matches a BloomAPI data source name (e.g. 'usgov.hhs.npi')
+* `id` a string that will be used to match the ID of the dataset (e.g. the ten-digit unique NPI or an HCPCS code)
+* `callback` a function that will be called once results retrieved. Parameters to callback include
+  *  `error` an object that is set if something went wrong
+  *  `response` an single result found given id
+
+# Example Usage
+
+The following will work on both the browser (using JSONP behind the scenes) and Node.js:
+
+**Create a new client**
+
+    // Basic
+    var bloomClient = new bloomjs.Client();
+    
+    // If using an API Key
+    var bloomClient = new bloomjs.Client('<API Key Here>');
+    
+    // If using your own server (with or without your own API keys)
+    var bloomClient = new bloomjs.Client({
+        url: "http://www.bloomapi.com/api",
+        apiKey: "<API Key Here>" // Optional
+    });
+
+**Query available datasources**
+
+      bloomClient.sources(function (error, response) {
+        if (error) return console.log(error.stack);
+        var source = response[0];
+        console.log('=== First available source');
+        console.log('Name: ' + source.source);
+        console.log('Last Checked for Updates: ' + source.checked);
+      });
+
+**Search the National Provider Identifier (NPI) without parameters**
+
+      bloomClient.search('usgov.hhs.npi', function (error, response, info) {
+        if (error) return console.log(error.stack);
+        console.log('=== First available NPI');
+        console.log('NPI: ' + response[0].npi);
+        console.log('Total NPIs: ' + info['meta']['rowCount']);
+      });
+
+**Search HCPCS Procedure Codes for the code 'C9289'**
+
+      bloomClient.search('usgov.hhs.hcpcs', {
+        'code': 'C9289'
+      }, function (error, response) {
+        if (error) return console.log(error.stack);
+        console.log('=== HCPCS code found');
+        console.log('Code: ' + response[0].code);
+      });
+
+**Search HCPCS Procedure Codes for any code with word that starts with 'ambula' in the description'**
+This also specifies an offset and limit using the options parameter.
+
+      bloomClient.search('usgov.hhs.hcpcs', {
+          'long_description': {
+            'prefix': 'ambula'
+          }
+        }, {
+          'offset': 0,
+          'limit': 10
+        }, function (error, response) {
+          if (error) return console.log(error.stack);
+          console.log('=== HCPCS code starting with ambula');
+          console.log('Code: ' + response[0].code);
+        });
+
+**Search HCPCS for both code 'L8410' and 'C9289' (Logical OR)**
+
+      bloomClient.search('usgov.hhs.hcpcs', {
+        'code': ['L8410', 'C9289']
+      }, function (error, response) {
+        if (error) return console.log(error.stack);
+        console.log('=== HCPCS code with codes L8410 or C9289');
+        console.log('Code: ' + response[0].code);
+        console.log('Code: ' + response[1].code);
+      });
+
+** Find a National Provider Identifier its NPI**
+
+    bloomClient.find('usgov.hhs.hcpcs', 1770707127, function (error, response) {
+        if (error) return console.log(error.stack);
+        console.log('=== NPI details for 1770707127');
+    });
---
+++ src/bloom.js
@@ -0,0 +1,257 @@
+(function (exports) {
+  "use strict";
+
+  exports.Client = function (options) {
+    this.url = "http://www.bloomapi.com/api";
+    this.defaultOptions = {
+      "offset": 0,
+      "limit": 20
+    };
+
+    if (typeof options === "string") {
+      this.key = options;
+    } else if (typeof options === "object" && options !== null) {
+      
+      if (typeof options.apiKey !== "undefined") {
+        this.key = options.apiKey;
+      }
+
+      if (typeof options.url !== "undefined") {
+        this.url = options.url;
+      }
+
+    }
+  };
+
+  exports.Client.prototype = {
+    sources: function (cb) {
+      var parameters = "", callUrl;
+
+      if (typeof this.apiKey !== "undefined") {
+        if (parameters.length > 0) {
+          parameters += "&";
+        }
+
+        parameters += "secret=" + this.apiKey;
+      }
+      
+      callUrl = this.url + "/sources" + (typeof parameters !== "undefined" ? "?" + parameters : "");
+      
+      sendRequest(callUrl, function (err, result) {
+        if (err) {
+          return cb(err);
+        }
+
+        return cb(null, result.result, { "meta": result.meta });
+      });
+    },
+    search: function (source) {
+      var options = {},
+          query, cb, parameters, callUrl;
+
+      for (var optAttr in this.defaultOptions) { options[optAttr] = this.defaultOptions[optAttr]; }
+      
+      if (typeof arguments[arguments.length - 1] === "function") {
+        cb = arguments[arguments.length - 1];
+      }
+
+      if (typeof arguments[1] === "object" && arguments[1] !== null) {
+        query = arguments[1];
+        try {
+          parameters = toParameters(query);
+        } catch (e) {
+          if (cb) {
+            cb(e);
+          }
+
+          return;
+        }
+      }
+
+      if (typeof arguments[2] === "object" && arguments[2] !== null) {
+        for (var addAttr in arguments[2]) { options[addAttr] = arguments[2][addAttr]; }
+
+        for (var attr in options) {
+          if (parameters.length > 0) {
+            parameters += "&";
+          }
+
+          parameters += attr + "=" + options[attr];
+        }
+      }
+
+      if (typeof this.apiKey !== "undefined") {
+        if (parameters.length > 0) {
+          parameters += "&";
+        }
+
+        parameters += "secret=" + this.apiKey;
+      }
+
+      callUrl = this.url + "/search/" + source + (typeof parameters !== "undefined" ? "?" + parameters : "");
+
+      sendRequest(callUrl, function (err, result) {
+        if (err) {
+          return cb(err);
+        }
+
+        return cb(null, result.result, { "meta": result.meta });
+      });
+    },
+    find: function (source, id, cb) {
+      var parameters = "", callUrl;
+
+      if (typeof this.apiKey !== "undefined") {
+        if (parameters.length > 0) {
+          parameters += "&";
+        }
+
+        parameters += "secret=" + this.apiKey;
+      }
+      
+      callUrl = this.url + "/sources/" + source + "/" + id + (typeof parameters !== "undefined" ? "?" + parameters : "");
+      
+      sendRequest(callUrl, function (err, result) {
+        if (err) {
+          return cb(err);
+        }
+
+        return cb(null, result.result, { "meta": result.meta });
+      });
+    }
+  };
+
+  var uniqueJSONP = 0;
+  function sendRequest(fullUrl, callback) {
+    var agent = "BloomJS/0.0.1";
+
+    if (typeof window !== "undefined") {
+      // Assume in browser
+      var name = "_jsonp_" + uniqueJSONP,
+          script;
+
+      uniqueJSONP += 1;
+
+      if (fullUrl.match(/\?/)) {
+        fullUrl += "&callback=" + name;
+      } else {
+        fullUrl += "?callback=" + name;
+      }
+      
+      script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = fullUrl;
+      
+      // Setup handler
+      window[name] = function (data) {
+        callback.call(window, null, data);
+        document.getElementsByTagName("head")[0].removeChild(script);
+        script = null;
+        delete window[name];
+      };
+      
+      // Load JSON
+      document.getElementsByTagName("head")[0].appendChild(script);
+    } else {
+      // Assume in nodejs
+      var http = require("http"),
+          https = require("https"),
+          url  = require("url");
+
+      var parsed, protocol;
+
+      parsed = url.parse(fullUrl);
+
+      protocol = (parsed.protocol === "https:" ? https : http);
+
+      protocol.get({
+        headers: {
+          "User-Agent": agent
+        },
+        hostname: parsed.hostname,
+        port: parsed.port,
+        path: parsed.path
+      }, function (res) {
+        var body = "";
+
+        if (res.statusCode !== 200 && res.statusCode !== 304) {
+          return callback(new Error("Non-OK http return: " + res.statusCode));
+        }
+
+        res.on("data", function (d) {
+          body += d;
+        });
+
+        res.on("end", function () {
+          try {
+            var result = JSON.parse(body);
+            callback(null, result);
+          } catch (e) {
+            return callback(e);
+          }
+        });
+      }).on("error", function (e) {
+        return callback(e);
+      });
+    }
+  }
+
+  function toParameters (query) {
+    var parameters = "",
+        index = 1;
+
+    for (var key in query) {
+      if (parameters.length > 0) {
+        parameters += "&";
+      }
+
+      if (typeof query[key] === "string") {
+        parameters += "key" + index + "=" + key;
+        parameters += "&op" + index + "=eq";
+        parameters += "&value" + index + "=" + query[key];
+
+        index += 1;
+      } else if (query[key] instanceof Array) {
+        var orIndex = 0,
+            values = query[key];
+
+        parameters += "key" + index + "=" + key;
+        parameters += "&op" + index + "=eq";
+        
+        for (; orIndex < values.length; orIndex += 1) {
+          parameters += "&value" + index + "=" + values[orIndex];
+        }
+
+        index += 1;
+      } else if (typeof query[key] === "object" && typeof query[key] !== null) {
+        for (var op in query[key]) {
+          if (typeof query[key][op] === "string") {
+            parameters += "key" + index + "=" + key;
+            parameters += "&op" + index + "=" + op;
+            parameters += "&value" + index + "=" + query[key][op];
+
+            index += 1;
+          } else if (query[key][op] instanceof Array) {
+            var nestOrIndex = 0,
+                nestValues = query[key][op];
+            parameters += "key" + index + "=" + key;
+            parameters += "&op" + index + "=" + op;
+
+            for (; nestOrIndex < nestValues.length; nestOrIndex += 1) {
+              parameters += "&value" + index + "=" + nestValues[nestOrIndex];
+            }
+
+            index += 1;
+          } else {
+            throw new Error("Unexpected query structure");
+          }
+        }
+      } else {
+        throw new Error("Unexpected query structure");
+      }
+    }
+
+    return parameters;
+  }
+
+})(typeof exports === "undefined" ? window.bloomjs = {} : exports);
